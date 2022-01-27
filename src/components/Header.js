16e100676bf8cfa91d2a26501ba0d61a6202e20d
yebin Park <yebinpark@yebinui-iMac.local>
2022-01-27 12:14:06 +0900

    redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import React, {useState} from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from "../features/car/carSlice"
+import { useSelector } from "react-redux";
 
 function Header() {
   const [burgerState, setBurgerStatus] = useState(false);
-  
-   
+  const cars= useSelector(selectCars);  
 
   return (
     <Container>
@@ -14,10 +15,9 @@ function Header() {
         <img src='/images/logo.svg' alt='logo' />
       </a>
       <Menu>
-        <a href='#'>Model S</a>
-        <a href='#'>Model 3</a>
-        <a href='#'>Model X</a>
-        <a href='#'>Model Y</a>
+        { cars && cars.map((car, index)=>
+          <a key={index} href='#'>{car}</a>
+        )}
       </Menu>
       <RightMenu>
         <a href='#'>Shop</a>
@@ -28,6 +28,9 @@ function Header() {
         <CloseWrapper>
           <CustomClose onClick={()=>setBurgerStatus(false)} />
         </CloseWrapper>
+        {cars && cars.map((car, index)=>
+          <li key={index}><a href='#'>{car}</a></li>
+        )}
         <li><a href='#'>Existing Inventory</a></li>
         <li><a href='#'>Used Inventory</a></li>
         <li><a href='#'>Trade-in</a></li>
